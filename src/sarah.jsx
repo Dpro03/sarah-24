c9557eede282c13496409bbdc19e226b9ef88a78
David Proto <davidproto@sbcglobal.net>
2023-02-04 22:45:23 -0500

    changed text

--- src/sarah.jsx
+++ src/sarah.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default () => (
     <div className="birthdayCard">
       <div className="cardFront">
@@ -13,9 +14,10 @@ export default () => (
         <h3 className="back">HAPPY BIRTHDAY!!!</h3>
         <p>Dear Sarah,<br></br><br></br>
         
-          Happy 24th Birthday!! I hope your day is filled with lots of love, fun and
-          laughter! May all your dreams and wishes come true! I love you so much and
-          I am so proud of you! I hope you have a wonderful day! 
+          Happy 24th Birthday!! It is so awesome to have a daughter like
+          you!  I hope your day is filled with lots of love, fun and
+          laughter!  I love you so much and
+          I am so proud of you! I hope you have an amazing Birthday! 
         </p>
         <p className="name">Love, Dad</p>
       </div>
