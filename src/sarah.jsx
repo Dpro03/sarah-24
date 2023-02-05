8a0f22fcaef98fea6c40deb50b25fa2bf65b05fa
David Proto <davidproto@sbcglobal.net>
2023-02-04 21:13:33 -0500

    first commit

---
+++ src/sarah.jsx
@@ -0,0 +1,25 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY SARAH!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY!!!</h3>
+        <p>Dear Sarah,<br></br><br></br>
+        
+          Happy 24th Birthday!! I hope your day is filled with lots of love, fun and
+          laughter! May all your dreams and wishes come true! I love you so much and
+          I am so proud of you! I hope you have a wonderful day! 
+        </p>
+        <p className="name">Love, Dad</p>
+      </div>
+    </div>
+  );
+
+  
